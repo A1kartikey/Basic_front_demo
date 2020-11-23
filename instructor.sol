8a38c559dd37bad62779e7e0aa5f8c9ee58675db
Kartikey <kartikey.dav100@gmail.com>
2020-11-24 01:39:32 +0530

    first commit

---
+++ instructor.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.1;
+
+contract Instructor {
+    
+   string fName;
+   uint age;
+
+event  Event(string fname,uint age);
+
+   function setInstructor(string memory _fName, uint _age) public {
+       fName = _fName;
+       age = _age;
+     
+       emit Event (_fName,_age);
+
+   }
+   
+   function getInstructor() view public returns (string memory, uint) {
+       return (fName, age);
+   }
+   
+}
